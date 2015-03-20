1cdf70e1006b0a8ace161a54d2d675092b1431fd
nmuende34 <mnmuende@live.com>
2015-03-19 17:35:05 -0700

    Final Commit

---
+++ Default.aspx
@@ -0,0 +1,28 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="_Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Time of Day</title>
+    <link href="StyleSheet.css" rel="stylesheet" type="text/css" />
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        <h1>Time of Day</h1>
+        <asp:Label ID="lblTimeSpan" runat="server" Text=""></asp:Label><br />
+        <asp:Image ID="Image1" runat="server" ImageUrl="~/image1.jpg" visible="false"/>
+        <asp:Image ID ="Image2" runat="server" ImageUrl="~/image2.jpg" visible ="false"/>
+        <asp:Image ID ="Image3" runat="server" ImageUrl="~/image3.jpg" visible="false" />
+        <asp:ScriptManager ID="ScriptManager1" runat="server"></asp:ScriptManager>
+        <asp:UpdatePanel ID="UpdatePanel1" runat="server">
+            <ContentTemplate>
+                <asp:Timer ID="Timer1" runat="server" Enabled="true" OnTick="Timer1_Tick" Interval="1000"></asp:Timer>
+                <asp:Label ID="lblTime" runat="server" Text=""></asp:Label>
+            </ContentTemplate>
+        </asp:UpdatePanel>
+    </div>
+    </form>
+</body>
+</html>
---
+++ Default.aspx.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class _Default : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        TimeSpan start = new TimeSpan(0, 0, 0);
+        TimeSpan morning = new TimeSpan(12, 0, 0);
+        TimeSpan afternoon = new TimeSpan(17, 0, 0);
+        TimeSpan end = new TimeSpan(23, 59, 59);
+        TimeSpan now = DateTime.Now.TimeOfDay;
+
+        if ((now > start) && (now < morning))
+        {
+            Image1.Visible = false;
+            Image2.Visible = false;
+            Image3.Visible = true;
+            lblTimeSpan.Text = "Good Morning!";
+        }
+        else if ((now > morning) && (now < afternoon))
+        {
+            Image1.Visible = true;
+            Image2.Visible = false;
+            Image3.Visible = false;
+            lblTimeSpan.Text = "Good Afternoon!";
+
+        }
+        else if (now > afternoon)
+        {
+            Image1.Visible = false;
+            Image2.Visible = true;
+            Image3.Visible = false;
+            lblTimeSpan.Text = "Good Evening!";        
+        }
+        else
+        {
+            Image1.Visible = true;
+            Image2.Visible = true;
+            Image3.Visible = true;
+        }
+    }
+
+    protected void Timer1_Tick(object sender, EventArgs e)
+    {
+        TimeService.TimeClockServiceClient ts = new TimeService.TimeClockServiceClient();
+        lblTime.Text = ts.GetCurrentTime();
+
+        
+    }
+}
